7f4d5406769d69ea75422c0b5d51a28be62b77ea
dstock3 <dstock3@gmail.com>
2023-03-21 22:03:58 -0500

    refactored event listener in copyPaste.js to account for multiple chat windows that are added throughout a session

--- static/js/copyPaste.js
+++ static/js/copyPaste.js
@@ -1,29 +1,23 @@
-const copyButtons = Array.from(document.querySelectorAll('.copy'));
+function copyText(button) {
+    const messageElement = button.parentElement;
+    const span = messageElement.querySelector('span');
+    const feedback = messageElement.querySelector('.copy-feedback');
+    const textToCopy = messageElement.textContent.replace(span.textContent, '').replace(button.textContent, '').replace(feedback.textContent, '').trim();
 
-copyButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        const messageElement = this.parentElement;
-        const span = messageElement.querySelector('span');
-        const button = this;
-        const feedback = messageElement.querySelector('.copy-feedback');
-        const textToCopy = messageElement.textContent.replace(span.textContent, '').replace(button.textContent, '').replace(feedback.textContent, '').trim();
+    const textArea = document.createElement('textarea');
+    textArea.value = textToCopy;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
 
-        const textArea = document.createElement('textarea');
-        textArea.value = textToCopy;
-        document.body.appendChild(textArea);
-        textArea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textArea);
+    const messageSpan = messageElement.querySelector('.copy-feedback');
+    messageSpan.classList.add('visible');
+    setTimeout(() => {
+        messageSpan.classList.remove('visible');
+    }, 2000);
 
-        const messageSpan = messageElement.querySelector('.copy-feedback');
-        messageSpan.classList.add('visible');
-        setTimeout(() => {
-            messageSpan.classList.remove('visible');
-        }, 2000);
-        
-        button.textContent = '✓';
-
-    });
-});
+    button.textContent = '✓';
+}
 
 
