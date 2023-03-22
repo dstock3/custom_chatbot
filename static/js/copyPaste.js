b9e926e893e5f7e805bf6f4bd4e8dae1411c6256
dstock3 <dstock3@gmail.com>
2023-03-21 22:17:36 -0500

    revised copy button

--- static/js/copyPaste.js
+++ static/js/copyPaste.js
@@ -2,7 +2,7 @@ const copyText = button => {
     const messageElement = button.parentElement;
     const span = messageElement.querySelector('span');
     const feedback = messageElement.querySelector('.copy-feedback');
-    const textToCopy = messageElement.textContent.replace(span.textContent, '').replace(button.textContent, '').replace(feedback.textContent, '').trim();
+    const textToCopy = messageElement.textContent.replace(span.textContent, '').replace(feedback.textContent, '').trim();
 
     const textArea = document.createElement('textarea');
     textArea.value = textToCopy;
