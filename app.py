d4d10a020d827753f15530880ee35ad86973d57b
dstock3 <dstock3@gmail.com>
2023-03-29 22:38:27 -0500

    Revert "in the process of refining user model"
    This reverts commit e1c6767db58a550b11c9ae16657cfca09615096e.

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 from assistant import main
 from model.database import insert_transcript, get_all_transcripts, init_db, delete_all_transcripts
-from model.user import create_user, get_user, update_user_preferences, init_user_table
-from intel.personalities import personalities
+from model.user import create_user, get_user, update_user_preferences
+from intel import personalities
 
 app = Flask(__name__)
-init_db(app)
-init_user_table(app)
+#init_db(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -43,17 +42,11 @@ def preferences():
     user = get_user()
 
     if request.method == 'POST':
-        update_user_preferences(
-            user['user_id'],
-            name=request.form.get('username'),
-            voice_command=request.form.get('voice_command'),
-            voice_response=request.form.get('voice_response'),
-            personality=request.form.get('personality'),
-        )
-    elif not user['name'] or not user['voice_command'] or not user['voice_response'] or not user['personality']:
-        # Redirect to the preferences page if the user has not set their preferences yet
-        return redirect(url_for('preferences'))
-    print(personalities)
+        user['username'] = request.form.get('username')
+        user['voice_command'] = request.form.get('voice_command')
+        user['voice_response'] = request.form.get('voice_response')
+        user['personality'] = request.form.get('personality')
+
     return render_template('preferences.html', user=user, personality_options=personalities)
 
 if __name__ == '__main__':
--- model/user.py
+++ model/user.py
@@ -1,30 +1,14 @@
 from model.database import get_db
 
-def init_user_table(app):
-    with app.app_context():
-        db = get_db()
-        db.execute("DROP TABLE IF EXISTS users")
-        db.execute('''CREATE TABLE users
-                     (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     name TEXT NOT NULL,
-                     voice_command BOOLEAN NOT NULL,
-                     voice_response BOOLEAN NOT NULL,
-                     personality TEXT NOT NULL);''')
-        db.commit()
-
-def create_user(name, voice_command=True, voice_response=True, personality="quirky"):
+def create_user(name, voice_command=None, voice_response=None, personality=None):
     db = get_db()
     db.execute("INSERT INTO users (name, voice_command, voice_response, personality) VALUES (?, ?, ?, ?)",
                  (name, voice_command, voice_response, personality))
     db.commit()
 
-def get_user(user_id=None):
+def get_user(user_id):
     db = get_db()
-    if user_id:
-        cursor = db.execute("SELECT user_id, name, voice_command, voice_response, personality FROM users WHERE user_id = ?", (user_id,))
-    else:
-        cursor = db.execute("SELECT user_id, name, voice_command, voice_response, personality FROM users LIMIT 1")
-
+    cursor = db.execute("SELECT user_id, name, voice_command, voice_response, personality FROM users WHERE user_id = ?", (user_id,))
     row = cursor.fetchone()
     if row:
         return {
@@ -34,12 +18,7 @@ def get_user(user_id=None):
             'voice_response': row[3],
             'personality': row[4]
         }
-    else:
-        if user_id:
-            return None
-        else:
-            create_user(name="New User")
-            return get_user()
+    return None
 
 def update_user_preferences(user_id, name=None, voice_command=None, voice_response=None, personality=None):
     db = get_db()
