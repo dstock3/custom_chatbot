e53018c726a7521b04432972e5d55f45b34f17db
dstock3 <dstock3@gmail.com>
2023-04-07 23:33:57 -0500

    revised search function to account for cases where no results are found.

--- static/js/search.js
+++ static/js/search.js
@@ -1,29 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const searchForm = document.getElementById("search-form");
-    const searchInput = document.getElementById("search");
-    const tableRows = document.querySelectorAll(".history-section table tr");
-  
-    searchForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      const searchTerm = searchInput.value.toLowerCase().trim();
-  
-      // loop through all table rows and hide those that don't match the search term
-      for (let i = 1; i < tableRows.length; i++) {
-        const row = tableRows[i];
+document.getElementById('search-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const searchInput = document.getElementById('search').value.trim().toLowerCase();
+    const searchRows = document.querySelectorAll('.search-row');
+    let noResultsMessage = document.getElementById('no-results-message');
+    let resultsFound = false;
+
+    searchRows.forEach(row => {
         const userMessage = row.cells[1].innerText.toLowerCase();
         const assistantMessage = row.cells[2].innerText.toLowerCase();
         const keywords = row.cells[3].innerText.toLowerCase();
-  
-        if (
-          userMessage.includes(searchTerm) ||
-          assistantMessage.includes(searchTerm) ||
-          keywords.includes(searchTerm)
-        ) {
-          row.style.display = "";
+
+        if (userMessage.includes(searchInput) || assistantMessage.includes(searchInput) || keywords.includes(searchInput)) {
+            row.style.display = '';
+            resultsFound = true;
         } else {
-          row.style.display = "none";
+            row.style.display = 'none';
         }
-      }
     });
-  });
+
+    if (!resultsFound) {
+        if (!noResultsMessage) {
+            noResultsMessage = document.createElement('p');
+            noResultsMessage.id = 'no-results-message';
+            noResultsMessage.innerText = 'No results found';
+            const historySection = document.querySelector('.history-section');
+            historySection.appendChild(noResultsMessage);
+        }
+    } else {
+        if (noResultsMessage) {
+            noResultsMessage.remove();
+        }
+    }
+});
