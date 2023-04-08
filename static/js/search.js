a150d9aae631ff9dc7bcb7baac5bd293571752b9
David Stockdale <dstock3@gmail.com>
2023-04-08 15:46:27 -0500

    added functionality for user to search history by date.

--- static/js/search.js
+++ static/js/search.js
@@ -1,29 +1,37 @@
 const searchHistory = (searchInput) => {
+    searchInput = searchInput.trim().toLowerCase();
     const searchRows = document.querySelectorAll('.search-row');
     let noResultsMessage = document.getElementById('no-results-message');
     let resultsFound = false;
 
-    const matchesSearch = (text) => text.toLowerCase().includes(searchInput.toLowerCase());
-
     searchRows.forEach(row => {
-        const userMessage = row.querySelector('.user-msg-item').innerText;
-        const assistantMessage = row.querySelector('.system-msg-item').innerText;
-        const keywords = Array.from(row.querySelectorAll('.keyword-item li')).map(li => li.innerText);
+        const userMessage = row.querySelector('.user-msg-item').innerText.toLowerCase();
+        const assistantMessage = row.querySelector('.system-msg-item').innerText.toLowerCase();
+        const keywords = Array.from(row.querySelectorAll('.keyword-item li')).map(li => li.innerText.toLowerCase());
+        const dateElement = row.querySelector('.date-item');
+        const dateString = dateElement.dataset.date;
+        const formattedDate = formatDate(dateString).toLowerCase();
 
-        const matchFound = [userMessage, assistantMessage, ...keywords].some(matchesSearch);
-
-        row.style.display = matchFound ? '' : 'none';
-        resultsFound = resultsFound || matchFound;
+        if (userMessage.includes(searchInput) || assistantMessage.includes(searchInput) || keywords.some(keyword => keyword.includes(searchInput)) || formattedDate.includes(searchInput)) {
+            row.style.display = '';
+            resultsFound = true;
+        } else {
+            row.style.display = 'none';
+        }
     });
 
-    if (!resultsFound && !noResultsMessage) {
-        noResultsMessage = document.createElement('p');
-        noResultsMessage.id = 'no-results-message';
-        noResultsMessage.innerText = 'No results found';
-        const historySection = document.querySelector('.history');
-        historySection.appendChild(noResultsMessage);
-    } else if (resultsFound && noResultsMessage) {
-        noResultsMessage.remove();
+    if (!resultsFound) {
+        if (!noResultsMessage) {
+            noResultsMessage = document.createElement('p');
+            noResultsMessage.id = 'no-results-message';
+            noResultsMessage.innerText = 'No results found';
+            const historySection = document.querySelector('.history');
+            historySection.appendChild(noResultsMessage);
+        }
+    } else {
+        if (noResultsMessage) {
+            noResultsMessage.remove();
+        }
     }
 };
 
@@ -39,4 +47,24 @@ document.querySelectorAll('.keyword-item li').forEach(keywordElement => {
         document.getElementById('search').value = keyword;
         searchHistory(keyword);
     });
+});
+
+const formatDate = (dateString) => {
+    const [year, month, day] = dateString.split(' ')[0].split('-');
+    const date = new Date(year, month - 1, day);
+    const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    const monthName = monthNames[date.getMonth()];
+    const formattedDate = `${monthName} ${date.getDate()}, ${date.getFullYear()}`;
+
+    return formattedDate;
+};
+
+document.querySelectorAll('.date-item').forEach(dateElement => {
+    const dateString = dateElement.dataset.date;
+    const formattedDate = formatDate(dateString);
+    dateElement.innerText = formattedDate;
 });
