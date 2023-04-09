c12f2e8a2ad83b52df7e2305d3175b2728d4daf6
dstock3 <dstock3@gmail.com>
2023-04-09 12:20:44 -0500

    revised extract_keywords function to include more broader array of keywords

--- assistant.py
+++ assistant.py
@@ -22,15 +22,16 @@ nlp = spacy.load('en_core_web_lg')
 
 def extract_keywords(text):
     doc = nlp(text)
+    
     keywords = set()
-
+    
     for ent in doc.ents:
         if ent.label_ in ('PERSON', 'ORG', 'GPE', 'FAC', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART'):
             keywords.add(ent.text)
 
     for token in doc:
-        if token.is_stop or token.is_punct or token.pos_ not in ('NOUN', 'PROPN', 'VERB'):
-            continue
+        if token.pos_ in ('NOUN', 'PROPN'):
+            keywords.add(token.text)
 
         if token.ent_type_ == 0:
             keywords.add(token.lemma_)
