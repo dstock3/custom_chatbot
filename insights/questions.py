6d85d8b205819185c4ba27ccf26a984cc9f51540
dstock3 <dstock3@gmail.com>
2023-04-13 00:03:57 -0500

    working on personality test.

--- insights/questions.py
+++ insights/questions.py
@@ -2,24 +2,113 @@ questions = {
     "basics": [
         {
             "id": "basic1",
-            "text": "What is your age?"
+            "text": "What is your age?",
+            "type": "text"
         },
         {
             "id": "basic2",
-            "text": "What is your gender?"
+            "text": "What is your gender?",
+            "type": "select",
+            "options": ["Male", "Female", "Non-binary", "Prefer not to say", "Other"]
         },
-        # Add more basic questions as needed
+        {
+            "id": "basic3",
+            "text": "What is your highest level of education?",
+            "type": "select",
+            "options": ["No formal education", "Primary education", "Secondary education", "Vocational/technical training", "Bachelor's degree", "Master's degree", "Doctorate or higher", "Other"]
+        },
+        {
+            "id": "basic4",
+            "text": "What is your current employment status?",
+            "type": "select",
+            "options": ["Employed full-time", "Employed part-time", "Self-employed", "Unemployed", "Student", "Retired", "Homemaker", "Other"]
+        },
+        {
+            "id": "basic5",
+            "text": "What is your annual income range?",
+            "type": "select",
+            "options": ["Under $25,000", "$25,000 - $49,999", "$50,000 - $74,999", "$75,000 - $99,999", "$100,000 - $149,999", "$150,000 - $199,999", "$200,000 and above", "Prefer not to say"]
+        },
+        {
+            "id": "basic6",
+            "text": "What is your country of residence?",
+            "type": "select",
+            "options": ["Country 1", "Country 2", "Country 3", "..."]  # Replace with a list of actual countries
+        },
+        {
+            "id": "basic7",
+            "text": "What is your ethnicity?",
+            "type": "select",
+            "options": ["Ethnicity 1", "Ethnicity 2", "Ethnicity 3", "..."]  # Replace with a list of actual ethnicities
+        },
+        {
+            "id": "basic8",
+            "text": "What is your relationship status?",
+            "type": "select",
+            "options": ["Single", "Married", "In a relationship", "Divorced", "Widowed", "Separated", "It's complicated", "Prefer not to say"]
+        },
     ],
     "personality": [
         {
-            "id": "personality1",
-            "text": "Do you consider yourself introverted or extroverted?"
+            "id": "openness1",
+            "text": "I have a vivid imagination.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
         },
         {
-            "id": "personality2",
-            "text": "What are your favorite hobbies?"
+            "id": "openness2",
+            "text": "I am interested in abstract ideas.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
         },
-        # Add more personality questions as needed
+        {
+            "id": "conscientiousness1",
+            "text": "I am always prepared.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "conscientiousness2",
+            "text": "I am a reliable worker.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "extraversion1",
+            "text": "I am the life of the party.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "extraversion2",
+            "text": "I feel comfortable around people.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "agreeableness1",
+            "text": "I am helpful and unselfish with others.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "agreeableness2",
+            "text": "I have a good relationship with my neighbors.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "neuroticism1",
+            "text": "I often feel sad.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
+        {
+            "id": "neuroticism2",
+            "text": "I get stressed out easily.",
+            "type": "select",
+            "options": ["1 - Strongly disagree", "2 - Disagree", "3 - Neutral", "4 - Agree", "5 - Strongly agree"]
+        },
     ],
     "health": [
         {
