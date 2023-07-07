b84ed4abdf523b05a14aabe0eaa79953a8e523b2
dstock3 <dstock3@gmail.com>
2023-07-07 17:57:05 -0500

    added style selector for assistant code headed. revised appearance of assistant code element

--- static/js/codeDetection.js
+++ static/js/codeDetection.js
@@ -5,19 +5,20 @@ assistantMessages.forEach((message) => {
     const codeSplit = message.innerHTML.split("%%%CODE_START%%%");
     const codeEndSplit = codeSplit[1].split("%%%CODE_END%%%");
     
-    const languaageSplit = message.innerHTML.split("%%%LANGUAGE_START%%%");
-    const languageEndSplit = languaageSplit[1].split("%%%LANGUAGE_END%%%");
-
-    const language = languageEndSplit[0];
-    
     const codeContainer = document.createElement("pre");
     codeContainer.classList.add("assistant-code");
     codeContainer.innerHTML = codeEndSplit[0];
 
-    const codeBlockHeader = document.createElement("div");
-    codeBlockHeader.classList.add("assistant-code-header");
-    codeBlockHeader.innerHTML = language;
-    codeContainer.prepend(codeBlockHeader);
+    if (message.innerHTML.includes("%%%LANGUAGE_START%%%")) {
+      const languaageSplit = message.innerHTML.split("%%%LANGUAGE_START%%%");
+      const languageEndSplit = languaageSplit[1].split("%%%LANGUAGE_END%%%");
+      const language = languageEndSplit[0];
+
+      const codeBlockHeader = document.createElement("div");
+      codeBlockHeader.classList.add("assistant-code-header");
+      codeBlockHeader.innerHTML = language;
+      codeContainer.prepend(codeBlockHeader);
+    }
 
     const spanStartIndex = codeSplit[0].indexOf("<span>");
     const spanEndIndex = codeSplit[0].indexOf("</span>") + 7;
