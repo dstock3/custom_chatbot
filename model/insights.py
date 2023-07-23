a1f30f151d9bf6ec0700f2619f0e12cbe8b91071
David Stockdale <dstock3@gmail.com>
2023-07-23 16:37:40 -0500

    revised insights model to align with object alluded to in insights template.

--- model/insights.py
+++ model/insights.py
@@ -1,14 +1,14 @@
 from model.database import get_db
 import json
 
-def save_user_response(user_id, question_id, response):
+def save_user_response(user_id, question_id, response, section):
     db = get_db()
     cursor = db.cursor()
 
     cursor.execute("""
-        INSERT INTO user_responses (user_id, question_id, response)
-        VALUES (?, ?, ?)
-    """, (user_id, question_id, response))
+        INSERT INTO user_responses (user_id, question_id, response, section)
+        VALUES (?, ?, ?, ?)
+    """, (user_id, question_id, response, section))
     db.commit()
 
 def get_user_responses(user_id):
@@ -16,14 +16,27 @@ def get_user_responses(user_id):
     cursor = db.cursor()
 
     cursor.execute("""
-        SELECT question_id, response
+        SELECT question_id, response, section
         FROM user_responses
         WHERE user_id=?
     """, (user_id,))
     
     responses = cursor.fetchall()
+    
+    insights = {
+        'basic': {},
+        'health': {},
+        'fam': {},
+        'work': {},
+        'big5': {},
+    }
 
-    return [{"question_id": row[0], "response": row[1]} for row in responses]
+    for row in responses:
+        section = row[2]
+        if section in insights:
+            insights[section][row[0]] = row[1]
+    
+    return insights
 
 def save_personality_score(user_id, big_five_scores):
     """
@@ -33,18 +46,16 @@ def save_personality_score(user_id, big_five_scores):
         user_id (str): The unique identifier of the user.
         big_five_scores (dict): A dictionary of the Big Five personality scores.
     """
-    # Create a dictionary to store user data
-    user_data = {
-        'user_id': user_id,
-        'big_five_scores': big_five_scores
-    }
+    db = get_db()
+    cursor = db.cursor()
     
-    # Serialize the data to a JSON formatted string
-    data = json.dumps(user_data)
+    cursor.execute("""
+        UPDATE user_responses
+        SET response = ?
+        WHERE user_id = ? AND section = 'big5'
+    """, (json.dumps(big_five_scores), user_id))
     
-    # Write the data to a file
-    file_name = f"user_{user_id}_personality_scores.json"
-    with open(file_name, 'w') as file:
-        file.write(data)
+    db.commit()
+
 
 
