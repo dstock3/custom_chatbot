9de2ea366dfe6fb8659bcf8e0d74c9e8b6d9f68a
David Stockdale <dstock3@gmail.com>
2023-07-27 14:54:47 -0500

    made stylistic adjustments to insights page.

--- static/js/insightsDropdown.js
+++ static/js/insightsDropdown.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var buttons = document.querySelectorAll(".dropdown-button");
+    const buttons = document.querySelectorAll(".dropdown-button");
 
     buttons.forEach(function(button) {
         button.addEventListener("click", function() {
-            var content = this.nextElementSibling;
+            const content = this.nextElementSibling;
             content.style.display = content.style.display === "none" ? "block" : "none";
         });
     });
