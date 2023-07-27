535976b391d2380cdba60192bd4d7329b552029b
David Stockdale <dstock3@gmail.com>
2023-07-26 20:50:01 -0500

    Added api call to big5 function.

--- insights/determine_personality.py
+++ insights/determine_personality.py
@@ -1,10 +1,11 @@
+import openai
+
 def big5_results(data):
     traits = ['openness', 'conscientiousness', 'extraversion', 'agreeableness', 'neuroticism']
-
     scores = {trait: 0 for trait in traits}
 
     for response in data:
-        trait_type = response['type'][:-1] 
+        trait_type = response['type'][:-1]
         answer = int(response['response'][0])
 
         if trait_type in scores:
@@ -22,5 +23,32 @@ def big5_results(data):
             insights[trait] = "High"
         else:
             insights[trait] = "Very high"
-    
-    return insights
+
+    insight_text = ', '.join(f'{trait}: {description}' for trait, description in insights.items())
+    prompt = (f"I've analyzed the individual's responses on the Big Five personality traits. "
+              f"Here are the insights: {insight_text}. "
+              f"Please provide key insights to optimize the individual's life, "
+              f"and formulate the insights as though you are speaking directly to them.")
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a data analyst with a specialty in psychology and sociology."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        max_tokens=250,
+        n=1,
+        stop=["Assistant:", "User:"],
+        temperature=.7,
+    )
+
+    summary = response['choices'][0]['message']['content'] if response['choices'] else ""
+
+    return summary
+
