92818583fae50ae56ec3faa5235e5d70d2fcf925
David Stockdale <dstock3@gmail.com>
2023-08-08 09:29:49 -0500

    Consolidated analysis functions in intel/analysis.py. Currently have regular analysis running every 5 interactions and in-depth analysis every 8 interactions. May need to tweak.

--- app.py
+++ app.py
@@ -47,10 +47,8 @@ def subject():
     history = get_all_transcripts()
 
     if request.method == 'POST':
-        analysis(user, transcript)
-        if len(transcript[2]) > 10:
-            insights = get_insights(user['user_id'])
-            indepth_analysis(insights, user, transcript)
+        insights = get_insights(user['user_id'])
+        analysis(insights, user, transcript)
         chat_transcript, display, auto_prompt, subject = processPOST(request, user, subject=subject)
         return render_template('index.html', chat_transcript=chat_transcript, display=display, history=history, user=user, auto_prompt=auto_prompt)
 
--- intel/analysis.py
+++ intel/analysis.py
@@ -1,7 +1,7 @@
 from intel.openai_call import apiCall
 from model.intel import insert_analysis
 
-def analysis(user, transcript):
+def analysis(insights, user, transcript):
     # we perform an analysis every 5 messages
     if (len(transcript[2]) % 5 != 0):
         messages = [
@@ -10,14 +10,12 @@ def analysis(user, transcript):
         ]
         analysis = apiCall(messages, 300, 0.8)
         insert_analysis(user["user_id"], transcript[0], analysis)
-
-def indepth_analysis(insights, user, transcript):
-    # need to determine when indepth analysis is needed
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant that performs analysis based on data provided to you."},
-        {"role": "user", "content": f"Perform an analysis of the user in following conversation and compare it to what you already know about this user. Here's what you already know: {insights} Now, what insights can be observed from the user's language and behavior? {transcript[2]}"}
-    ]
-    analysis = apiCall(messages, 500, 0.9)
-    insert_analysis(user["user_id"], transcript[0], analysis)
+    elif (len(transcript[2]) % 8 != 0):
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant that performs analysis based on data provided to you."},
+            {"role": "user", "content": f"Perform an analysis of the user in following conversation and compare it to what you already know about this user. Here's what you already know: {insights} Now, what insights can be observed from the user's language and behavior? {transcript[2]}"}
+        ]
+        analysis = apiCall(messages, 500, 0.9)
+        insert_analysis(user["user_id"], transcript[0], analysis)
     
 
