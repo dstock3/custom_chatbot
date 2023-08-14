d54bd96ed6c7d25d3eb2ac33b12f72e577f29bb7
David Stockdale <dstock3@gmail.com>
2023-08-14 11:33:00 -0500

    added some notation to search_history model.

--- model/search_history.py
+++ model/search_history.py
@@ -6,11 +6,13 @@ def add_search_history(user_id, search_text):
     db.commit()
 
 def get_search_history(user_id):
+    #need to enable user to view search history in user preferences
     db = get_db()
     cur = db.execute('SELECT search_text, search_date FROM search_history WHERE user_id = ? ORDER BY search_date DESC', (user_id,))
     return cur.fetchall()
 
 def delete_search_history(user_id, search_text):
+    #need to enable user to delete search history in user preferences
     db = get_db()
     db.execute('DELETE FROM search_history WHERE user_id = ? AND search_text = ?', (user_id, search_text))
     db.commit()
