bdc4a01563c5f9359cf5f1c2ed8a5d5e79b9c1c4
David Stockdale <dstock3@gmail.com>
2023-09-13 21:08:06 -0500

    added ul element to notes with keywords

--- static/js/notes.js
+++ static/js/notes.js
@@ -15,17 +15,40 @@ document.getElementById('showNotesModalBtn').addEventListener('click', function(
     fetch('/notes')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             const notesList = document.querySelector('.notes-list');
             notesList.innerHTML = ''; 
 
             data[0].forEach(note => {
-                const noteItem = document.createElement('div');
-                noteItem.className = 'note-item ' + data[1];
-                noteItem.innerHTML = `
+                let noteContent = `
                     <p class="note-date">${formatDate(note[4])}</p>
                     <p class="note-content">${note[2]}</p>
-                `;
+                `
+
+                let tags;
+                try {
+                    tags = JSON.parse(note[3]);
+                } catch(e) {
+                    const lines = note[3].split('\n');
+                    tags = lines.map(line => line.replace(/^"|"$/g, '').trim());
+                }
+            
+                const ulElement = document.createElement('ul'); 
+                ulElement.className = 'note-tags';
+            
+                for (let i = 0; i < tags.length; i++) {
+                    console.log(tags[i]);
+                    
+                    const liElement = document.createElement('li');
+                    liElement.className = 'note-tag ' + data[1] + '-accent';;  
+                    liElement.innerText = tags[i];                   
+                    ulElement.appendChild(liElement);                
+                }
+                
+                const noteItem = document.createElement('div');
+                noteItem.className = 'note-item ' + data[1];
+                noteItem.innerHTML = noteContent;
+            
+                noteItem.appendChild(ulElement); 
                 notesList.appendChild(noteItem);
             });
             
